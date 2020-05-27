f60c3acf221cff2f41041cd682b8e1462e6e7fae
ws <ws_dev@163.com>
2020-05-27 11:33:40 +0800

    up

--- AutofacAsyncInterceptor-More/MethodInvoke/CallLoggerAsyncInterceptor.cs
+++ AutofacAsyncInterceptor-More/MethodInvoke/CallLoggerAsyncInterceptor.cs
@@ -33,16 +33,20 @@ namespace AutofacAsyncInterceptor
             Console.WriteLine("Proceed Begins");
             invocation.Proceed();
             Console.WriteLine("Proceed Ends");
-            var taskType = invocation.Method.ReturnType;//warnning:this line with make the task run
+            var taskType = invocation.Method.ReturnType;
+            Console.WriteLine("Intercept log 01");
             var resultType = taskType.GetGenericArguments()[0];
+            Console.WriteLine("Intercept log 02");
             var mi = HandleAsyncWithResultMethodInfo.MakeGenericMethod(resultType);
+            Console.WriteLine("Intercept log 03");
             invocation.ReturnValue = mi.Invoke(this, new[] { invocation.ReturnValue, invocation });
+            Console.WriteLine("Intercept log 04");
         }
 
 
         private async Task<T> HandleAsyncWithResult<T>(Task<T> task, IInvocation invocation)
         {
-            Console.WriteLine("AutofacAsyncInterceptor HandleAsyncWithResult<T> Before await");
+            Console.WriteLine("AutofacAsyncInterceptor HandleAsyncWithResult<T> Before await"); //error: the task runs eariler than this line
             var result = await task;
             Console.WriteLine("AutofacAsyncInterceptor HandleAsyncWithResult<T> After await");
             if (IsEnabled(invocation))
