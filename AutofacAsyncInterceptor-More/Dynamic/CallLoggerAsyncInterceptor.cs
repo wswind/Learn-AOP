5c8c82f93f4acc21b174540870313e0b3de3276b
ws <ws_dev@163.com>
2020-05-27 17:21:11 +0800

    up

--- AutofacAsyncInterceptor-More/Dynamic/CallLoggerAsyncInterceptor.cs
+++ AutofacAsyncInterceptor-More/Dynamic/CallLoggerAsyncInterceptor.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 //https://stackoverflow.com/a/39784559/7726468
--- AutofacAsyncInterceptor/CallLoggerAsyncInterceptor.cs
+++ AutofacAsyncInterceptor/CallLoggerAsyncInterceptor.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 //https://github.com/JSkimming/Castle.Core.AsyncInterceptor/issues/42
 namespace AutofacAsyncInterceptor
 {
+#if USE_INTERCEPTOR_BASE
+
     public class CallLoggerAsyncInterceptor : AsyncInterceptorBase  
     {
         TextWriter _output;
@@ -52,5 +54,67 @@ namespace AutofacAsyncInterceptor
         {
             throw new NotImplementedException();
         }
+
     }
+#else
+    public class CallLoggerAsyncInterceptor : IAsyncInterceptor
+    {
+        TextWriter _output;
+
+        public CallLoggerAsyncInterceptor(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void InterceptAsynchronous(IInvocation invocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterceptAsynchronous<TResult>(IInvocation invocation)
+        {
+            invocation.ReturnValue = InternalInterceptAsynchronous<TResult>(invocation);
+        }
+
+        public void InterceptSynchronous(IInvocation invocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<TResult> InternalInterceptAsynchronous<TResult>(IInvocation invocation)
+        {
+            Console.WriteLine("Begin Intercept");
+
+            _output.WriteLine("Calling method '{0}' with parameters '{1}'... ",
+                invocation.Method.Name,
+                string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+
+            //check enable log
+            bool isEnabled = AttributeHelper.IsLoggerEnabled(invocation.Method);
+
+            //before invocation
+            if (isEnabled)
+            {
+                _output.WriteLine("Logger is Enabled");
+            }
+            //invocation
+
+            Console.WriteLine("before proceed");
+            invocation.Proceed();
+            var task = (Task<TResult>)invocation.ReturnValue;
+            var result = await task;
+            Console.WriteLine("after proceed");
+
+            //after invocation
+            if (isEnabled)
+            {
+                _output.WriteLine("Done: result was '{0}'.", result);
+            }
+            //invocation.ReturnValue = Task.FromResult("changed value");
+            Console.WriteLine("End Intercept");
+            return result;
+        }
+    }
+
+#endif
 }
