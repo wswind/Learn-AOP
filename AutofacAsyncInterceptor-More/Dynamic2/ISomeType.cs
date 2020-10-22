48e77855388ba0b8c97558290decef694d427fad
ws <ws_dev@163.com>
2020-10-22 17:30:26 +0800

    add demo with lightwind

---
+++ AutofacAsyncInterceptor-More/Dynamic2/ISomeType.cs
@@ -0,0 +1,12 @@
+﻿using Autofac.Extras.DynamicProxy;
+using System.Threading.Tasks;
+
+namespace AutofacAsyncInterceptor
+{
+    public interface ISomeType
+    {
+        Task<string> ShowAsyncWithReturnValue(string input);
+        Task ShowAsync(string input);
+        void ShowSynchronous(string input);
+    }
+}
