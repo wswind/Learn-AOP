254bc57ad5f80f9f698614de600713c495c2a94d
ws <ws_dev@163.com>
2021-03-04 16:41:51 +0800

    up

--- CastleUse/Program.cs
+++ CastleUse/Program.cs
@@ -7,10 +7,13 @@ namespace CastleUse
         static void Main(string[] args)
         {
             var proxyGenerator = new ProxyGenerator();
+            //the intercepted function must be virtual
             var svc = proxyGenerator.CreateClassProxy<MessageClient>(new MyInterceptorAspect());
             svc.Send("hi");
-
+            
             var svc2 = proxyGenerator.CreateInterfaceProxyWithTarget<IHelloClient>(new HelloClient(), new MyInterceptorAspect());
+            //difference between CreateInterfaceProxyWithTargetInterface see:
+            //https://kozmic.net/2009/11/13/interfaceproxywithtarget-interfaceproxywithtargetinterface-ndash-whatrsquos-the-difference/
             svc2.Hello();
         }
     }
